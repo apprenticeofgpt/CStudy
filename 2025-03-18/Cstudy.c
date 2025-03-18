b6c019257e198e13c7b70051339181bbc6ffd6e9
apprenticeofgpt <maddux0782@gmail.com>
2025-03-18 16:55:00 +0900

    11

--- 2025-03-18/Cstudy.c
+++ 2025-03-18/Cstudy.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 int main()
 {
-	printf("Hello, world!\n");
+	int a, b, result;
+	scanf_s("%d %d", &a, &b);
+
+
+	result = a + b;
+	printf("%d + %d = %d \n", a, b, result);
+
+	result = a - b;
+	printf("%d - %d = %d \n", a, b, result);
+
+	result = a * b;
+	printf("%d * %d = %d \n", a, b, result);
+
+	result = a / b;	
+	printf("%d / %d = %d", a, b, result);	
+
 }
